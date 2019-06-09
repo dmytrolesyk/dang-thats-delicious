0d1094124943a6f0b70a6a2053a65a1bf47983a5
dmytrolesyk <dmitriylesik@gmail.com>
2019-06-09 16:27:55 +0300

    working on registration

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,33 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      return next(null, true);
+    }
+    next({ message: `That filetype isn't allowed` }, false);
+  },
+};
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  if(!req.file) {
+    return next();
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
+};
 
 exports.homePage = (req, res) => {
   res.render('index');
@@ -12,5 +40,46 @@ exports.addStore = (req, res) => {
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
-  res.redirect(`/store/${store.slug}`);
+  res.redirect(`/stores/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  const { id: _id } = req.params;
+  const store = await Store.findOne({ _id });
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  const { id: _id } = req.params;
+  req.body.location.type = 'Point';
+  const store = await Store.findOneAndUpdate({ _id }, req.body, {
+    new: true,
+    runValidators: true,
+  }).exec();
+  req.flash(
+    'success',
+    `Store ${store.name} was successfully updated.
+    <a href="/stores/${store.slug}">View Store</a>`
+  );
+  res.redirect('/stores');
+};
+
+exports.getStoreBySlug = async (req,  res, next) => {
+  const { slug } = req.params;
+  const store = await Store.findOne({ slug });
+  if (!store) return next();
+  res.render('store', { store, title: store.name });
+};
+
+exports.getStoresByTag = async (req, res) => {
+  const { tag } = req.params;
+  const tagQuery = tag || { $exists: true };
+  const stores = await Store.find({ tags: tagQuery });
+  const render = tags => res.render('tags', { tags, tag, stores, title:  'Tags' });
+  Store.getTagsList(render);
 };
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', storeController.getStores);
+router.get('/stores/:slug', catchErrors(storeController.getStoreBySlug));
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
 router.get('/add', storeController.addStore);
-router.post('/add', catchErrors(storeController.createStore));
+router.post('/add',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore),
+);
+
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+router.post('/add/:id',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore),
+);
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+router.post('/register', userController.validateRegister);
 
 module.exports = router;
